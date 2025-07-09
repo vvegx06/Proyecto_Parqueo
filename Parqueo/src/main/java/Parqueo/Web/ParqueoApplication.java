ca28d53fbd8ed9e5184f7552c59ba0b58e6c42df
Andres Vega <andresvegamunoz2206@gmail.com>
2025-07-08 23:00:44 -0600

    Commit1

---
+++ Parqueo/src/main/java/Parqueo/Web/ParqueoApplication.java
@@ -0,0 +1,13 @@
+package Parqueo.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueoApplication.class, args);
+	}
+
+}
---
+++ Parqueo/src/test/java/Parqueo/Web/ParqueoApplicationTests.java
@@ -0,0 +1,13 @@
+package Parqueo.Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
